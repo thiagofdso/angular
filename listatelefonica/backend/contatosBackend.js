3c1d54ccdb9abc96904058b1a5c0b6cd548dc046
Thiago Fernandes <thiagofdso.ufpa@gmail.com>
2016-06-15 10:59:13 -0300

    Update contatosBackend.js

--- listatelefonica/backend/contatosBackend.js
+++ listatelefonica/backend/contatosBackend.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
-app.use(express.bodyParser());
+app.use(express.json());
+app.use(express.urlencoded());
 
 var contatos = [
 	{nome: "Bruno", telefone: "9999-2222", data: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}},
